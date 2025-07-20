0c8a116eaef8dd82a447c1b01d1903ba9cc5af64
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-07-20 17:40:11 +0200

    delink overlay 110

---
+++ include/ov110/PlayerGet.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "global.h"
+#include "types.h"
+
+#include "System/SysNew.hpp"
+
+class UnkStruct_ov110_021861ec {
+public:
+    /* 00 */ unk32 mUnk_00;
+    /* 04 */ unk32 mUnk_04;
+    /* 08 */ unk32 mUnk_08;
+    /* 0c */
+
+    ARM UnkStruct_ov110_021861ec(unk32 param1, unk32 param2, unk32 param3) {
+        this->mUnk_00 = param1;
+        this->mUnk_04 = param2;
+        this->mUnk_08 = param3;
+    }
+};
+
+class PlayerGet {
+public:
+    void func_ov110_02184a40();
+    void func_ov110_02184dac();
+    void func_ov110_02184ddc();
+    void func_ov110_02184e7c();
+    void func_ov110_02184f40();
+    void func_ov110_0218500c();
+    void func_ov110_021855e4();
+    void func_ov110_02185cb4();
+    void func_ov110_02185d30();
+    void func_ov110_02185d3c();
+    void func_ov110_02185da4();
+    void func_ov110_02185db4();
+};
+
+extern UnkStruct_ov110_021861ec data_ov110_021861ec;
---
+++ src/ov110/PlayerGet.cpp
@@ -0,0 +1,20 @@
+#include "ov110/PlayerGet.hpp"
+
+
+ARM void PlayerGet::func_ov110_02184a40() {}
+ARM void PlayerGet::func_ov110_02184dac() {}
+ARM void PlayerGet::func_ov110_02184ddc() {}
+ARM void PlayerGet::func_ov110_02184e7c() {}
+ARM void PlayerGet::func_ov110_02184f40() {}
+ARM void PlayerGet::func_ov110_0218500c() {}
+
+#pragma section force_data begin
+UnkStruct_ov110_021861ec data_ov110_021861ec = UnkStruct_ov110_021861ec(0x5E3, 0x152D, 0xCD);
+#pragma section force_data end
+
+ARM void PlayerGet::func_ov110_021855e4() {}
+ARM void PlayerGet::func_ov110_02185cb4() {}
+ARM void PlayerGet::func_ov110_02185d30() {}
+THUMB void PlayerGet::func_ov110_02185d3c() {}
+ARM void PlayerGet::func_ov110_02185da4() {}
+ARM void PlayerGet::func_ov110_02185db4() {}
