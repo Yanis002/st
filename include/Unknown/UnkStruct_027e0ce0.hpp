ac11ed00e57901f58661d57d9fc3af93af8e51e7
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-07-23 14:39:15 +0200

    document the item manager and UnkStruct_027e0ce0

--- include/Unknown/UnkStruct_027e0ce0.hpp
+++ include/Unknown/UnkStruct_027e0ce0.hpp
@@ -3,24 +3,33 @@
 #include "global.h"
 #include "types.h"
 
+#include "Item/ItemManager.hpp"
 #include "System/SysNew.hpp"
 
 // PlayerManager?
 class UnkStruct_027e0ce0 {
 public:
-    /* 00 */ unk32 mUnk_00;
+    /* 00 */ unk8 mHealth;
+    /* 04 */ unk8 mHealthMax;
+    /* 02 */ unk32 mUnk_02;
+    /* 03 */ unk32 mUnk_03;
     /* 04 */ unk32 mUnk_04;
-    /* 08 */ unk32 mUnk_08;
-    /* 0c */ unk32 mUnk_0c;
-    /* 10 */ unk32 mUnk_10;
-    /* 14 */ unk32 mUnk_14;
-    /* 18 */ unk32 mUnk_18;
-    /* 1c */ unk32 mUnk_1c;
-    /* 20 */ unk32 mUnk_20;
+    /* 08 */ unk32 *mUnk_08;
+    /* 0c */ unk32 *mUnk_0c;
+    /* 10 */ unk32 *mUnk_10;
+    /* 14 */ unk32 *mUnk_14;
+    /* 18 */ unk32 *mUnk_18;
+    /* 1c */ unk32 *mUnk_1c;
+    /* 20 */ unk32 *mUnk_20;
     /* 24 */ unk32 mUnk_24;
-    /* 28 */ unk32 mUnk_28;
-    /* 2c */ unk32 mUnk_2c;
-    /* 30 */
+    /* 28 */ ItemManager *mUnk_28;
+    /* 2c */ ItemManager *mUnk_2c; // same as above
+    /* 30 */ unk32 *mUnk_30; // ?
+    /* 34 */ unk32 *mUnk_34;
+    /* 38 */ unk32 *mUnk_38; // pointer to data_027e0478 when on the train
+    /* 3c */ unk32 mUnk_3c;
+    /* 40 */ unk32 *mUnk_40; // pointer to data_027e0478 when not on the train
+    /* 44 */
 
     void func_01fff148();
 
