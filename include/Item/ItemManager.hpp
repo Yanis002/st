8d16ba5dc9d7d8c31c6ab67bd65868d3d17b1043
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-08-03 00:20:10 +0200

    itemmanager progress

--- include/Item/ItemManager.hpp
+++ include/Item/ItemManager.hpp
@@ -10,7 +10,7 @@ public:
     /* 04 */ unk32 mForcedItem;
     /* 08 */ u32 mUnk_08[2]; // inventory items bitfield & collection/equipment bitfield
     /* 10 */ unk16 mNumRupees;
-    /* 12 */ unk16 mUnk_12; // "toggle bitfield"
+    /* 12 */ u16 mUnk_12; // "toggle bitfield"
     /* 14 */ unk16 mItemRestrictions; // bitfield
     /* 16 */ unk8 mTearsAmount; // number of tears of light
     /* 17 */ u8 mKeyAmount; // number of small keys
@@ -30,6 +30,8 @@ public:
     ~ItemManager();
 
     void func_ov000_020a865c(ItemFlag itemFlag);
+    bool func_ov000_020a8680();
+    unk32 func_ov000_020a86a4();
     u32 func_ov000_020a86d0(ItemFlag itemFlag);
     u8 func_ov000_020a8728();
     u8 func_ov000_020a8748();
@@ -45,5 +47,85 @@ public:
     unk32 func_ov000_020a9c90(TreasureType type);
     bool func_ov000_020a9ca4(TreasureType type);
     void func_ov000_020a9cbc(TreasureType type, s32 amount);
+    static unk32 func_ov000_020a9d78(unk32 param1);
+    static unk32 func_ov000_020a9e14(unk32 param1);
+    static unk32 func_ov000_020a9eb0(unk32 param1);
+    static unk32 func_ov000_020a9f4c(unk32 param1);
+    unk32 func_ov000_020aa02c(ItemId itemId);
     void func_ov000_020aa0ac(ItemId itemId);
+
+    //! TODO: find out if it belongs here (some of them are (derived) Actor vfuncs?)
+    void func_ov000_020a8974();
+    void func_ov000_020a8984();
+    void func_ov000_020a89bc();
+    void func_ov000_020a89d4();
+    void func_ov000_020a8a0c();
+    void func_ov000_020a8a5c();
+    void func_ov000_020a8a74();
+    void func_ov000_020a8a90();
+    void func_ov000_020a8aa4();
+    void func_ov000_020a8ab8();
+    void func_ov000_020a8acc();
+    void func_ov000_020a8ae0();
+    void func_ov000_020a8af4();
+    void func_ov000_020a8b48();
+    void func_ov000_020a8b7c();
+    void func_ov000_020a8bb0();
+    void func_ov000_020a8cc0();
+    void func_ov000_020a8ce0();
+    void func_ov000_020a8d08();
+    void func_ov000_020a8d28();
+    void func_ov000_020a8da0();
+    void func_ov000_020a8da4();
+    void func_ov000_020a8db0();
+    void func_ov000_020a8dd0();
+    void func_ov000_020a8df0();
+    void func_ov000_020a8e84();
+    void func_ov000_020a8e9c();
+    void func_ov000_020a8ec0();
+    unk32 func_ov000_020a8f54();
+    unk32 func_ov000_020a8f5c();
+    unk32 func_ov000_020a8f64();
+    void func_ov000_020a8f6c();
+    void func_ov000_020a8f78();
+    void func_ov000_020a8fe0();
+    void func_ov000_020a8ff4();
+    void func_ov000_020a914c();
+    void func_ov000_020a9150();
+    unk32 func_ov000_020a91a0();
+    unk32 func_ov000_020a91a8();
+    unk32 func_ov000_020a91b0();
+    void func_ov000_020a91b8();
+    void func_ov000_020a9200();
+    void func_ov000_020a921c();
+    void func_ov000_020a9240();
+    void func_ov000_020a9244();
+    void func_ov000_020a9248();
+    void func_ov000_020a92d0();
+    void func_ov000_020a9448();
+    void func_ov000_020a94b0();
+    void func_ov000_020a9598();
+    void func_ov000_020a95c4();
+    void func_ov000_020a95d4();
+    void func_ov000_020a95d8();
+    void func_ov000_020a95e4();
+    unk32 func_ov000_020a97fc();
+    void func_ov000_020a9804();
+    void func_ov000_020a98f0();
+    void func_ov000_020a98f4();
+    void func_ov000_020a995c();
+    void func_ov000_020a99a4();
+    void func_ov000_020a99d8();
+    void func_ov000_020a9a20();
+    void func_ov000_020a9a34();
+    void func_ov000_020a9a50();
+    void func_ov000_020a9a94();
+    void func_ov000_020a9abc();
+    void func_ov000_020a9ae0();
+    void func_ov000_020a9afc();
+    void func_ov000_020a9b10();
+    void func_ov000_020a9b2c();
+    void func_ov000_020a9b3c();
+    void func_ov000_020a9b4c();
+    void func_ov000_020a9c4c();
 };
--- src/000_Second/Item/ItemManager.cpp
+++ src/000_Second/Item/ItemManager.cpp
@@ -4,10 +4,27 @@
 // quiver and bomb bag tiers
 extern u8 data_ov000_020afc40[3];
 extern u8 data_ov000_020afc43[3];
+extern unk32 data_ov000_020afde8[];
 
 // removeEquipmentItem
 ARM void ItemManager::func_ov000_020a865c(ItemFlag itemFlag) {
     UNSET_FLAG(this->mUnk_08, itemFlag);
+}
+
+ARM bool ItemManager::func_ov000_020a8680() {
+    return GET_FLAG(this->mUnk_08, ItemFlag_RecruitUniform) && (this->mUnk_12 & 1);
+}
+
+ARM unk32 ItemManager::func_ov000_020a86a4() {
+    if (GET_FLAG(this->mUnk_08, ItemFlag_AncientShield) && (this->mUnk_12 & 2)) {
+        return 1;
+    }
+
+    if (GET_FLAG(this->mUnk_08, ItemFlag_Shield)) {
+        return 0;
+    }
+
+    return -1;
 }
 
 // getItemAmmo
@@ -145,6 +162,104 @@ ARM bool ItemManager::func_ov000_020a8948() {
     return true;
 }
 
+ARM void ItemManager::func_ov000_020a8974() {}
+ARM void ItemManager::func_ov000_020a8984() {}
+THUMB void ItemManager::func_ov000_020a89bc() {}
+ARM void ItemManager::func_ov000_020a89d4() {}
+ARM void ItemManager::func_ov000_020a8a0c() {}
+ARM void ItemManager::func_ov000_020a8a5c() {}
+ARM void ItemManager::func_ov000_020a8a74() {}
+ARM void ItemManager::func_ov000_020a8a90() {}
+ARM void ItemManager::func_ov000_020a8aa4() {}
+ARM void ItemManager::func_ov000_020a8ab8() {}
+ARM void ItemManager::func_ov000_020a8acc() {}
+ARM void ItemManager::func_ov000_020a8ae0() {}
+ARM void ItemManager::func_ov000_020a8af4() {}
+ARM void ItemManager::func_ov000_020a8b48() {}
+ARM void ItemManager::func_ov000_020a8b7c() {}
+ARM void ItemManager::func_ov000_020a8bb0() {}
+ARM void ItemManager::func_ov000_020a8cc0() {}
+ARM void ItemManager::func_ov000_020a8ce0() {}
+ARM void ItemManager::func_ov000_020a8d08() {}
+ARM void ItemManager::func_ov000_020a8d28() {}
+ARM void ItemManager::func_ov000_020a8da0() {}
+ARM void ItemManager::func_ov000_020a8da4() {}
+ARM void ItemManager::func_ov000_020a8db0() {}
+ARM void ItemManager::func_ov000_020a8dd0() {}
+ARM void ItemManager::func_ov000_020a8df0() {}
+ARM void ItemManager::func_ov000_020a8e84() {}
+ARM void ItemManager::func_ov000_020a8e9c() {}
+ARM void ItemManager::func_ov000_020a8ec0() {}
+
+ARM unk32 ItemManager::func_ov000_020a8f54() {
+    return 1;
+}
+
+ARM unk32 ItemManager::func_ov000_020a8f5c() {
+    return 1;
+}
+
+ARM unk32 ItemManager::func_ov000_020a8f64() {
+    return 0x7B;
+}
+
+ARM void ItemManager::func_ov000_020a8f6c() {}
+ARM void ItemManager::func_ov000_020a8f78() {}
+ARM void ItemManager::func_ov000_020a8fe0() {}
+ARM void ItemManager::func_ov000_020a8ff4() {}
+ARM void ItemManager::func_ov000_020a914c() {}
+ARM void ItemManager::func_ov000_020a9150() {}
+
+ARM unk32 ItemManager::func_ov000_020a91a0() {
+    return 1;
+}
+
+ARM unk32 ItemManager::func_ov000_020a91a8() {
+    return -1;
+}
+
+ARM unk32 ItemManager::func_ov000_020a91b0() {
+    return 1;
+}
+
+ARM void ItemManager::func_ov000_020a91b8() {}
+ARM void ItemManager::func_ov000_020a9200() {}
+ARM void ItemManager::func_ov000_020a921c() {}
+ARM void ItemManager::func_ov000_020a9240() {}
+ARM void ItemManager::func_ov000_020a9244() {}
+ARM void ItemManager::func_ov000_020a9248() {}
+ARM void ItemManager::func_ov000_020a92d0() {}
+ARM void ItemManager::func_ov000_020a9448() {}
+ARM void ItemManager::func_ov000_020a94b0() {}
+ARM void ItemManager::func_ov000_020a9598() {}
+ARM void ItemManager::func_ov000_020a95c4() {}
+ARM void ItemManager::func_ov000_020a95d4() {}
+ARM void ItemManager::func_ov000_020a95d8() {}
+ARM void ItemManager::func_ov000_020a95e4() {}
+
+ARM unk32 ItemManager::func_ov000_020a97fc() {
+    return 1;
+}
+
+ARM void ItemManager::func_ov000_020a9804() {}
+ARM void ItemManager::func_ov000_020a98f0() {}
+ARM void ItemManager::func_ov000_020a98f4() {}
+ARM void ItemManager::func_ov000_020a995c() {}
+ARM void ItemManager::func_ov000_020a99a4() {}
+ARM void ItemManager::func_ov000_020a99d8() {}
+ARM void ItemManager::func_ov000_020a9a20() {}
+ARM void ItemManager::func_ov000_020a9a34() {}
+ARM void ItemManager::func_ov000_020a9a50() {}
+ARM void ItemManager::func_ov000_020a9a94() {}
+ARM void ItemManager::func_ov000_020a9abc() {}
+ARM void ItemManager::func_ov000_020a9ae0() {}
+ARM void ItemManager::func_ov000_020a9afc() {}
+ARM void ItemManager::func_ov000_020a9b10() {}
+ARM void ItemManager::func_ov000_020a9b2c() {}
+ARM void ItemManager::func_ov000_020a9b3c() {}
+ARM void ItemManager::func_ov000_020a9b4c() {}
+ARM void ItemManager::func_ov000_020a9c4c() {}
+
 // hasAnyTreasure
 ARM bool ItemManager::func_ov000_020a9c64() {
     for (s32 i = 0; i < ARRAY_LEN(this->mUnk_3c); i++) {
@@ -187,6 +302,28 @@ ARM void ItemManager::func_ov000_020a9cbc(TreasureType type, s32 amount) {
     }
 
     this->mUnk_3c[type] = newAmount;
+}
+
+ARM unk32 ItemManager::func_ov000_020a9d78(unk32 param1) {}
+ARM unk32 ItemManager::func_ov000_020a9e14(unk32 param1) {}
+ARM unk32 ItemManager::func_ov000_020a9eb0(unk32 param1) {}
+ARM unk32 ItemManager::func_ov000_020a9f4c(unk32 param1) {}
+
+ARM unk32 ItemManager::func_ov000_020aa02c(ItemId itemId) {
+    switch (itemId) {
+        case ItemId_RandCommonTreasure:
+            return data_ov000_020afde8[ItemManager::func_ov000_020a9d78(-1)];
+        case ItemId_RandUncommonTreasure:
+            return data_ov000_020afde8[ItemManager::func_ov000_020a9e14(-1)];
+        case ItemId_RandRareTreasure:
+            return data_ov000_020afde8[ItemManager::func_ov000_020a9eb0(-1)];
+        case ItemId_RandLegendaryTreasure:
+            return data_ov000_020afde8[ItemManager::func_ov000_020a9f4c(-1)];
+        default:
+            break;
+    }
+
+    return itemId;
 }
 
 // gainTreasureFromItem
