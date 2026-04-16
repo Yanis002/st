754eb103086abdac4a6129685915129e566d7b75
Yanis <35189056+Yanis002@users.noreply.github.com>
2026-04-16 13:27:46 +0200

    Decompile `MapObject` functions (#51)
    * decompile MapObject
    
    * fix regression
    
    * match the remaining 4 functions

--- include/Unknown/UnkStruct_027e0cd8.hpp
+++ include/Unknown/UnkStruct_027e0cd8.hpp
@@ -85,6 +85,14 @@ public:
     bool func_ov000_02080658(unk32 param1, UnkStruct_SceneChange1 *param2);
     void func_ov000_020808f4(void *param1, void *param2, unk32 param3);
     unk32 func_ov000_02080a44();
+    void func_ov000_020801b0(Vec2b *param1, unk32 param2, unk32 param3);
+};
+
+class UnkStruct_027e0cd8_10 {
+public:
+    /* 00 */ unk32 mUnk_00;
+
+    void func_01fff6d0(Vec3p *param1, s32 *param2, s32 *param3);
 };
 
 class UnkStruct_027e0cd8 {
@@ -92,12 +100,16 @@ public:
     /* 00 */ unk32 mUnk_00;
     /* 04 */ unk32 *mUnk_04;
     /* 08 */ unk32 mUnk_08;
-    /* 0c */ UnkStruct_027e0cd8_0c *mUnk_0c;
-    /* 10 */
+    /* 0C */ UnkStruct_027e0cd8_0c *mUnk_0c;
+    /* 10 */ UnkStruct_027e0cd8_10 *mUnk_10;
+    /* 14 */ STRUCT_PAD(0x14, 0x4C);
+    /* 4C */
 
     unk32 func_ov000_02081e30(unk32 param1, unk32 param2);
     void func_ov000_02081d7c(unk32 param1, unk32 param2, unk32 param3);
     void func_ov000_02081ecc(unk16 param1, unk32 param2);
+    void func_ov000_02081eec(unk16 param1, unk32 param2, unk32 param3);
+    bool func_ov000_02081f3c(unk16 param1, unk32 param2);
 };
 
 extern UnkStruct_027e0cd8 *data_027e0cd8;
